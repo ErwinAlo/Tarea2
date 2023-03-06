ab9e88eb832c1c8448b15deaefd48f280fab560b
ErwinAlo <ealo2347@gmail.com>
2023-03-05 21:10:58 -0600

    Clase Puerta

---
+++ Puerta.cs
@@ -0,0 +1,17 @@
+ namespace Tarea2;
+     public class Puerta{
+        public string Color;
+        public Puerta(){
+           
+            Color = "cafe";
+        }
+        public void SetColor(string Color){
+
+               this.Color = Color; 
+        }
+        public void MostrarDatos(){
+
+               Console.WriteLine($"Soy una puerta mi color es: {Color}");
+        }
+     }
+     
